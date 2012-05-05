3d932a80e6e230b3b8d823acd3c2fdc320901e51
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-05-05 23:55:43 +0300

    this should do the trick

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-kirjoitusalusta_backups
-=======================
+# Backups of our pads from kirjoitusalusta.fi
+
+It seems the future of this great service is not 100% certain, so we dumped backups here.
+
+A small script to automate the dumping is included.
--- dump.sh
+++ dump.sh
@@ -1,4 +1,14 @@
-#!/bin/bash -ex
-wget --no-check-certificate -q -O /dev/stdout http://kirjoitusalusta.fi/ep/pad/export/hacklab/latest?format=html | sed -r -e "s%\"https?://kirjoitusalusta.fi/([^\"]+)\"%\"http://kirjoitusalusta.fi/ep/pad/export/\1/latest?format=html\"%g"  >hacklab/latest\?format\=html.html
+#!/bin/bash -x
+wget -E --no-check-certificate -q -O /dev/stdout http://kirjoitusalusta.fi/ep/pad/export/hacklab/latest?format=html | sed -r -e "s%\"https?://kirjoitusalusta.fi/([^\"]+)\"%\"http://kirjoitusalusta.fi/ep/pad/export/\1/latest?format=html\"%g"  >hacklab/latest\?format\=html.html
 wget -nH --cut-dirs=3 -E -k -r -np --force-html --no-check-certificate --base=http://kirjoitusalusta.fi --input-file hacklab/latest\?format\=html.html
 
+# rename the dumped files
+for file in $( find . -name '*.html' | grep 'latest' )
+do
+    mv -f "$file" `echo "$file" | sed -r -e "s%/latest.*\.html%.html%g"`
+    rmdir `echo "$file" | sed -r -e "s%/latest.*\.html%/%g"`
+done
+
+# Autocommit
+git add hacklab*.html
+git commit -m 'dumped with script'
